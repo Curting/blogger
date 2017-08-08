c75bb52a08be58c135d00086d0e9f94ca0811a2b
Oliver <oc@hejverden.dk>
2017-08-08 22:49:01 +0200

    Sorcery authentication complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
+  before_action :require_login, only: [ :new, :create, :edit, :update, :destroy ]
 
   def index
     @articles = Article.all 
